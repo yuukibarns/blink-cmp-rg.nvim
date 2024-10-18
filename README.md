98bf68dcf9de6ff3125f5089898716a26016a8a2
niuiic <niuiicde@gmail.com>
2024-10-19 00:19:15 +0800

    feat: config

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # blink-cmp-rg.nvim
 
 Ripgrep source for blink.nvim.
+
+```lua
+require("blink.cmp").setup({
+	sources = {
+		providers = {
+			{ "blink.cmp.sources.lsp", name = "LSP" },
+			{ "blink.cmp.sources.path", name = "Path", score_offset = 3 },
+			{ "blink.cmp.sources.snippets", name = "Snippets", score_offset = -3 },
+			{ "blink.cmp.sources.buffer", name = "Buffer", fallback_for = { "LSP" } },
+			{
+				"blink-cmp-rg",
+				name = "Rg",
+				-- options below are optional, these are the default values
+				prefix_min_len = 3,
+				get_command = function(prefix)
+					return {
+						"rg",
+						"--heading",
+						"--json",
+						"--word-regexp",
+						"--color",
+						"never",
+						prefix .. "[\\w_-]+",
+						vim.fs.root(0, "git") or vim.fn.getcwd(),
+					}
+				end,
+			},
+		},
+	},
+})
+
+```
--- lua/blink-cmp-rg/init.lua
+++ lua/blink-cmp-rg/init.lua
@@ -1,13 +1,20 @@
 local RgSource = {}
 
-function RgSource:new(config)
-	config = config or {}
+function RgSource.new(config)
 	return setmetatable({
 		prefix_min_len = config.prefix_min_len or 3,
-		get_root_dir = config.get_root_dir or function()
-			return vim.fs.root(0, "git") or vim.fn.getcwd()
+		get_command = config.get_command or function(prefix)
+			return {
+				"rg",
+				"--heading",
+				"--json",
+				"--word-regexp",
+				"--color",
+				"never",
+				prefix .. "[\\w_-]+",
+				vim.fs.root(0, "git") or vim.fn.getcwd(),
+			}
 		end,
-		word_pattern = config.word_pattern or "[\\w_-]+",
 	}, { __index = RgSource })
 end
 
@@ -19,53 +26,40 @@ function RgSource:get_completions(_, resolve)
 		return
 	end
 
-	vim.system(
-		{
-			"rg",
-			"--heading",
-			"--json",
-			"--word-regexp",
-			"--color",
-			"never",
-			prefix .. self.word_pattern,
-			self:get_root_dir(),
-		},
-		nil,
-		function(result)
-			if result.code ~= 0 then
-				resolve()
-				return
-			end
-
-			local items = {}
-			local lines = vim.split(result.stdout, "\n")
-			vim.iter(lines)
-				:map(function(line)
-					local ok, item = pcall(vim.json.decode, line)
-					return ok and item or {}
-				end)
-				:filter(function(item)
-					return item.type == "match"
-				end)
-				:map(function(item)
-					return item.data.submatches
-				end)
-				:flatten()
-				:each(function(submatch)
-					items[submatch.match.text] = {
-						label = submatch.match.text,
-						kind = vim.lsp.protocol.CompletionItemKind.Text,
-						insertText = submatch.match.text,
-					}
-				end)
-
-			resolve({
-				is_incomplete_forward = false,
-				is_incomplete_backward = false,
-				items = vim.tbl_values(items),
-			})
+	vim.system(self.get_command(prefix), nil, function(result)
+		if result.code ~= 0 then
+			resolve()
+			return
 		end
-	)
+
+		local items = {}
+		local lines = vim.split(result.stdout, "\n")
+		vim.iter(lines)
+			:map(function(line)
+				local ok, item = pcall(vim.json.decode, line)
+				return ok and item or {}
+			end)
+			:filter(function(item)
+				return item.type == "match"
+			end)
+			:map(function(item)
+				return item.data.submatches
+			end)
+			:flatten()
+			:each(function(submatch)
+				items[submatch.match.text] = {
+					label = submatch.match.text,
+					kind = vim.lsp.protocol.CompletionItemKind.Text,
+					insertText = submatch.match.text,
+				}
+			end)
+
+		resolve({
+			is_incomplete_forward = false,
+			is_incomplete_backward = false,
+			items = vim.tbl_values(items),
+		})
+	end)
 end
 
 return RgSource
